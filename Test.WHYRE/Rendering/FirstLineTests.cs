2980d152c72e2ef75394c12e265cabfb95662cb4
meerow <meerow@mirmx.de>
2023-11-17 17:36:49 +0100

    test: add Firstline Test

---
+++ Test.WHYRE/Rendering/FirstLineTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Tonga.IO;
+using Tonga.Text;
+using Xunit;
+
+namespace Whyre.Rendering.Test
+{
+	public sealed class FirstLineTests
+	{
+		[Fact]
+		public async void RendersFirstLine()
+		{
+			Assert.Equal(
+				"CONNECT /world HTTP/1.1",
+				await
+					new FirstLine()
+						.Refine("CONNECT /world HTTP/1.1")
+						.Render()
+			);
+		}
+	}
+}
+
