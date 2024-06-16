cc1e9365b9b4177f06496537de22e5a976dcad46
Meerow <meerow@mirmx.de>
2024-06-16 13:44:47 +0200

    feat: add MatchEnvelope

---
+++ src/WHyLL.Http/Response/Success.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using Tonga;
+using Tonga.Enumerable;
+using WHyLL.Message;
+
+namespace WHyLL.Http.Response
+{
+    /// <summary>
+    /// Success response.
+    /// </summary>
+    public sealed class Success : MessageEnvelope
+    {
+        /// <summary>
+        /// Success response.
+        /// </summary>
+        public Success() : base(
+            new SimpleMessage(
+                new ResponseLine((int)HttpStatusCode.OK),
+                new None<IPair<string, string>>(),
+                new MemoryStream(0)
+            )
+        )
+        { }
+    }
+}
+
---
+++ src/WHyLL/Rendering/MatchEnvelope.cs
@@ -0,0 +1,35 @@
+﻿using Tonga;
+
+namespace WHyLL.Rendering
+{
+    /// <summary>
+    /// Envelope for matches to use in <see cref="Switch{TOutput}"/>.
+    /// </summary>
+    public abstract class MatchEnvelope<TOutput> : IMatch<TOutput>
+    {
+        private readonly IMatch<TOutput> match;
+
+        /// <summary>
+        /// Envelope for matches to use in <see cref="Switch{TOutput}"/>.
+        /// </summary>
+        public MatchEnvelope(IMatch<TOutput> match)
+        {
+            this.match = match;
+        }
+
+        public IRendering<TOutput> Consequence(
+            string firstLine,
+            IEnumerable<IPair<string, string>> parts,
+            Stream body
+        ) =>
+            this.match.Consequence(firstLine, parts, body);
+
+        public bool Matches(
+            string firstLine,
+            IEnumerable<IPair<string, string>> parts,
+            Stream body
+        ) =>
+            this.match.Matches(firstLine, parts, body);
+    }
+}
+
